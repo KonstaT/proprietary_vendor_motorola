3b604f7a26fcd0f94e032e24e6ce790422f6a0cd
Konsta <konsta09@gmail.com>
2015-01-04 18:41:29 +0200

    falcon: update adreno blobs
    From bacon.
    
    <qeglDrvAPI_eglInitialize:410>: EGL 1.4 QUALCOMM build: Nondeterministic_AU_msm8974_LA.BF.1.1.1_RB1__release_AU ()
    OpenGL ES Shader Compiler Version: E031.25.03.00
    Build Date: 12/11/14 Thu
    Local Branch: mybranch6275889
    Remote Branch: quic/LA.BF.1.1_rb1.16
    Local Patches: NONE
    Reconstruct Branch: NOTHING
    
    Change-Id: I52517462e04e571cf48590a82220c2773cdc5431

--- falcon/falcon-vendor-blobs.mk
+++ falcon/falcon-vendor-blobs.mk
@@ -25,16 +25,14 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/falcon/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
     vendor/motorola/falcon/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-    vendor/motorola/falcon/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    vendor/motorola/falcon/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
-    vendor/motorola/falcon/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/motorola/falcon/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
+    vendor/motorola/falcon/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libscale.so:system/vendor/lib/libscale.so \
